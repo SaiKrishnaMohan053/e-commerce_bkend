609f9a4d8d88c91ddd930ed4ed64ea4e7feb6576
Sai Krishna Mohan <saikrishnamohan.k@gmail.com>
2023-11-26 12:59:35 -0600

    added page

--- routes/data.route.js
+++ routes/data.route.js
@@ -4,5 +4,8 @@ var dataCntrl = require('../controllers/data.controller');
 
 router.post('/signup', dataCntrl.saveData);
 router.post('/login', dataCntrl.login);
+router.get('/welcomePage', (req, res)=>{
+    res.send({message: 'intial page'});
+})
 
 module.exports = router;
